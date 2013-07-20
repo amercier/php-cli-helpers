0267a5284c232866d170d146e5396671d73ccab4
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-07-20 19:47:32 +0200

    Fixed ParameterTest to match latest assertScriptOutput API

--- tests/unit/ParameterTest.php
+++ tests/unit/ParameterTest.php
@@ -8,73 +8,61 @@ class ParameterTestCase extends AbstractCliScriptTestCase
 
     public function testShortRequiredParameter()
     {
-        $this->assertScriptOutput(self::SCRIPT . ' -u myname -p mypassword', implode("\n", array(
-                'array(4) {',
-                '    \'host\'     => \'127.0.0.1\',',
-                '    \'username\' => \'myusername\',',
-                '    \'password\' => \'mypassword\',',
-                '    \'verbose\'  => false,',
-                '}',
-            )));
+        $this->assertScriptOutput(self::SCRIPT . ' -u myname -p mypassword', array(
+                'host' => '127.0.0.1',
+                'username' => 'myname',
+                'password' => 'mypassword',
+                'verbose' => false
+            ));
     }
 
     public function testLongRequiredParameter()
     {
-        $this->assertScriptOutput(self::SCRIPT . ' --username myname --password mypassword', implode("\n", array(
-                'array(4) {',
-                '    \'host\'     => \'127.0.0.1\',',
-                '    \'username\' => \'myusername\',',
-                '    \'password\' => \'mypassword\',',
-                '    \'verbose\'  => false,',
-                '}',
-            )));
+        $this->assertScriptOutput(self::SCRIPT . ' --username myname --password mypassword', array(
+                'host' => '127.0.0.1',
+                'username' => 'myname',
+                'password' => 'mypassword',
+                'verbose' => false
+            ));
     }
 
     public function testShortOptionalParameter()
     {
-        $this->assertScriptOutput(self::SCRIPT . ' -u myname -p mypassword -h myserver.example.com', implode("\n", array(
-                'array(4) {',
-                '    \'host\'     => \'myserver.example.com\',',
-                '    \'username\' => \'myusername\',',
-                '    \'password\' => \'mypassword\',',
-                '    \'verbose\'  => false,',
-                '}',
-            )));
+        $this->assertScriptOutput(self::SCRIPT . ' -u myname -p mypassword -h myserver.example.com', array(
+                'host' => 'myserver.example.com',
+                'username' => 'myname',
+                'password' => 'mypassword',
+                'verbose' => false
+            ));
     }
 
     public function testLongOptionalParameter()
     {
-        $this->assertScriptOutput(self::SCRIPT . ' -u myname -p mypassword --host myserver.example.com', implode("\n", array(
-                'array(4) {',
-                '    \'host\'     => \'myserver.example.com\',',
-                '    \'username\' => \'myusername\',',
-                '    \'password\' => \'mypassword\',',
-                '    \'verbose\'  => false,',
-                '}',
-            )));
+        $this->assertScriptOutput(self::SCRIPT . ' -u myname -p mypassword --host myserver.example.com', array(
+                'host' => 'myserver.example.com',
+                'username' => 'myname',
+                'password' => 'mypassword',
+                'verbose' => false
+            ));
     }
 
     public function testShortSwitchParameters()
     {
-        $this->assertScriptOutput(self::SCRIPT . ' -u myname -p mypassword -v', implode("\n", array(
-                'array(4) {',
-                '    \'host\'     => \'127.0.0.1\',',
-                '    \'username\' => \'myusername\',',
-                '    \'password\' => \'mypassword\',',
-                '    \'verbose\'  => true,',
-                '}',
-            )));
+        $this->assertScriptOutput(self::SCRIPT . ' -u myname -p mypassword -v', array(
+                'host' => '127.0.0.1',
+                'username' => 'myname',
+                'password' => 'mypassword',
+                'verbose' => true
+            ));
     }
 
     public function testLongSwitchParameters()
     {
-        $this->assertScriptOutput(self::SCRIPT . ' -u myname -p mypassword --verbose', implode("\n", array(
-                'array(4) {',
-                '    \'host\'     => \'127.0.0.1\',',
-                '    \'username\' => \'myusername\',',
-                '    \'password\' => \'mypassword\',',
-                '    \'verbose\'  => true,',
-                '}',
-            )));
+        $this->assertScriptOutput(self::SCRIPT . ' -u myname -p mypassword --verbose', array(
+                'host' => '127.0.0.1',
+                'username' => 'myname',
+                'password' => 'mypassword',
+                'verbose' => true
+            ));
     }
 }
