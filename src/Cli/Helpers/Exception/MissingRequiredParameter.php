45ce6ceea114445a14edae62430b9cc61e1a2314
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-07-21 09:56:31 +0200

    Implemented Exception\MissingRequiredParameter

---
+++ src/Cli/Helpers/Exception/MissingRequiredParameter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Cli\Helpers\Exception;
+
+use Cli\Helpers\Exception as CliHelpersException;
+
+/**
+ * Exception that occurs with Parameter::getFromCommandLine() where a required
+ * parameter is missing. Ex:
+ *
+ *     my-script.php                (-u/--username missing)
+ */
+class MissingRequiredParameter extends CliHelpersException
+{
+    public function __construct($parameter)
+    {
+        global $argv;
+        parent::__construct('Missing parameter -' . $parameter->getShort() . '/--' . $parameter->getLong() . ' in command "php ' . implode(' ',$argv) . '"');
+    }
+
+}
