2362702f200b69b42695cff255d8ffe46501bb0b
Alexandre Mercier <amercier.ext@orange.com>
2014-03-28 16:56:02 +0100

    Fixed switches order

--- tests/integration/data/test-documented-script.php
+++ tests/integration/data/test-documented-script.php
@@ -10,10 +10,11 @@ $script
     ->setName('test-documented-script.php')
     ->setVersion('1.0')
     ->setDescription('Test script for Cli\Helpers\DocumentedScript')
-    ->addParameter(new Parameter('H', 'host'    , '127.0.0.1')              , 'Host.')
-    ->addParameter(new Parameter('u', 'username', Parameter::VALUE_REQUIRED), 'User name.')
-    ->addParameter(new Parameter('p', 'password', Parameter::VALUE_REQUIRED), 'Password.')
+    ->setCopyright('2014 Alexandre Mercier')
     ->addParameter(new Parameter('v', 'verbose' , Parameter::VALUE_NO_VALUE), 'Enable verbosity.')
+    ->addParameter(new Parameter('p', 'password', Parameter::VALUE_REQUIRED), 'Password.')
+    ->addParameter(new Parameter('u', 'username', Parameter::VALUE_REQUIRED), 'User name.')
+    ->addParameter(new Parameter('H', 'host'    , '127.0.0.1')              , 'Host.')
     ->setProgram(function ($options, $arguments) {
         var_dump($arguments);
         var_dump($options);
--- tests/integration/data/test-script.php
+++ tests/integration/data/test-script.php
@@ -10,10 +10,11 @@ $script
     ->setName('test-script.php')
     ->setVersion('1.0')
     ->setDescription('Test script for Cli\Helpers\Script')
-    ->addParameter(new Parameter('H', 'host'    , '127.0.0.1')              , 'Host.')
-    ->addParameter(new Parameter('u', 'username', Parameter::VALUE_REQUIRED), 'User name.')
-    ->addParameter(new Parameter('p', 'password', Parameter::VALUE_REQUIRED), 'Password.')
+    ->addCopyright('2014 Alexandre Mercier')
     ->addParameter(new Parameter('v', 'verbose' , Parameter::VALUE_NO_VALUE), 'Enable verbosity.')
+    ->addParameter(new Parameter('p', 'password', Parameter::VALUE_REQUIRED), 'Password.')
+    ->addParameter(new Parameter('u', 'username', Parameter::VALUE_REQUIRED), 'User name.')
+    ->addParameter(new Parameter('H', 'host'    , '127.0.0.1')              , 'Host.')
     ->setProgram(function ($options) {
         var_dump($options);
     })
