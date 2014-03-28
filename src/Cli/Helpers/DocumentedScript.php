b92c7c865a0218e1b98798bb072c1505ebdcdf35
Alexandre Mercier <amercier.ext@orange.com>
2014-03-28 15:37:24 +0100

    WIP on DocumentedScript

--- src/Cli/Helpers/DocumentedScript.php
+++ src/Cli/Helpers/DocumentedScript.php
@@ -6,27 +6,74 @@ class DocumentedScript extends Script
 {
     public function __construct()
     {
+        $this->addVersionParameter();
+        $this->addHelpParameter();
+    }
+
+    protected function addHelpParameter()
+    {
         $this->addParameter(
             new Parameter('h', 'help', Parameter::VALUE_NO_VALUE),
             'Output version information and exit.',
             function ($arguments) {
                 echo $this->name . ' v' . $this->version . "\n"
-                    . 'Copyright (c) ' . $this->copyright . "\n";
+                    . $this->copyright . "\n";
                 return false;
             }
         );
+    }
+
+    protected function addVersionParameter()
+    {
         $this->addParameter(
-            new Parameter('v', 'version', Parameter::VALUE_NO_VALUE),
+            new Parameter('V', 'version', Parameter::VALUE_NO_VALUE),
             'Display this help and exit.',
             function ($arguments) {
-                echo 'Usage: ' . $arguments[0] . "[OPTIONS]\n"
+                echo 'Usage: ' . $arguments[0];
+
+                $options = array();
+                foreach (array_reverse($this->parameters, true) as $id => $parameter) {
+
+                    // Show in syntax line if required
+                    if ($parameter->getDefaultValue() === Parameter::VALUE_REQUIRED) {
+                        echo ' ' . $parameter->getShortSwitch()
+                            . ' ' . str_replace('-', '_', strtoupper($parameter->getLong()));
+                    }
+
+                    // Add to options
+                    $options[] = array(
+                        '  ' . $parameter->getShortSwitch() . ', ' . $parameter->getLongSwitch(),
+                        (
+                            $parameter->getDefaultValue() !== Parameter::VALUE_NO_VALUE
+                            ? str_replace('-', '_', strtoupper($parameter->getLong()))
+                            : ''
+                        ),
+                        '  ',
+                        (
+                            $parameter->getDefaultValue() !== Parameter::VALUE_REQUIRED
+                            && $parameter->getDefaultValue() !== Parameter::VALUE_NO_VALUE
+                            ? preg_replace(
+                                '/(\\.)?$/',
+                                ' (defaults to \'' . $parameter->getDefaultValue() . '\')$1',
+                                $this->parameterDescriptions[$id],
+                                1
+                            )
+                            : $this->parameterDescriptions[$id]
+                        ),
+                    );
+                }
+
+                echo " [OPTIONS]\n"
                     . "\n"
                     . $this->description . "\n"
+                    . "\n"
+                    . IO::strPadAll($options, array(), "\n", ' ', ' ', false) . "\n"
                     . "\n"
                     . $this->name . ' v' . $this->version . "\n"
                     . 'Copyright (c) ' . $this->copyright . "\n";
                 return false;
             }
         );
+
     }
 }
--- tests/unit/DocumentedScriptTest.php
+++ tests/unit/DocumentedScriptTest.php
@@ -5,79 +5,48 @@ use Cli\Helpers\Parameter;
 
 class DocumentedScriptUnitTestCase extends PHPUnit_Framework_TestCase
 {
-    // protected $helloWorld;
+    protected $helloWorld;
 
-    // public function setUp()
-    // {
-    //     $this->helloWorld = DocumentedScript::create()
-    //         ->setExceptionCatchingEnabled(false)
-    //         ->setName('Hello')
-    //         ->setVersion('1.0')
-    //         ->setDescription('Say hello to the world or to a particular person')
-    //         ->setCopyright('Copyright (c) Orange ECV 2013')
-    //         ->addParameter(new Parameter('n', 'name'   , 'World'                  ), 'Set the name of the person to greet')
-    //         ->addParameter(new Parameter('V', 'verbose', Parameter::VALUE_NO_VALUE), 'Increase verbosity')
-    //         ->setProgram(function($options) {
-    //             if ($options['name'] === 'Adolf') {
-    //                 throw new \Exception('That name is prohibited');
-    //             }
-    //             echo 'Hello, ' . $options['name'] . '!';
-    //             if ($options['verbose']) {
-    //                 echo ' Nice to see you again :)';
-    //             }
-    //             echo "\n";
-    //             return 'OK';
-    //         });
-    // }
-
-    // public function testVersionWithShortSwitch() {
-    //     $result = $this->helloWorld->start(array('script.php', '-v'));
-    //     $this->expectOutputString(
-    //         "Usage: script.php [OPTIONS]\n"
-    //         . "\n"
-    //         . "Say hello to the world or to a particular person\n"
-    //         . "\n"
-    //         . "  -h, --help       Display this help and exit.\n"
-    //         . "  -v, --version    Output version information and exit.\n"
-    //         . "\n"
-    //         . "Hello v1.0\n"
-    //         . "Copyright (c) Copyright (c) Orange ECV 2013\n"
-    //     );
-    // }
-
-    // public function testVersionWithLongSwitch() {
-    //     $result = $this->helloWorld->start(array('script.php', '--version'));
-    //     $this->expectOutputString(
-    //         "Usage: script.php [OPTIONS]\n"
-    //         . "\n"
-    //         . "Say hello to the world or to a particular person\n"
-    //         . "\n"
-    //         . "  -h, --help       Display this help and exit.\n"
-    //         . "  -v, --version    Output version information and exit.\n"
-    //         . "\n"
-    //         . "Hello v1.0\n"
-    //         . "Copyright (c) Copyright (c) Orange ECV 2013\n"
-    //     );
-    // }
-
-    // public function testHelpWithShortSwitch() {
-    //     $result = $this->helloWorld->start(array('script.php', '-h'));
-    //     $this->expectOutputString(
-    //         "Hello v1.0\n"
-    //         . "Copyright (c) Copyright (c) Orange ECV 2013\n"
-    //     );
-    // }
-
-    // public function testHelpWithLongSwitch() {
-    //     $result = $this->helloWorld->start(array('script.php', '--help'));
-    //     $this->expectOutputString(
-    //         "Hello v1.0\n"
-    //         . "Copyright (c) Copyright (c) Orange ECV 2013\n"
-    //     );
-    // }
-    //
-    public function testOK()
+    public function setUp()
     {
-        $this->assertEquals(true, true);
+        $this->helloWorld = new DocumentedScript();
+        $this->helloWorld
+            ->setExceptionCatchingEnabled(false)
+            ->setName('Hello')
+            ->setVersion('1.0')
+            ->setDescription('Say hello to the world or to a particular person')
+            ->setCopyright('Copyright (c) Orange ECV 2013')
+            ->addParameter(new Parameter('p', 'password', Parameter::VALUE_REQUIRED), 'The password.')
+            ->addParameter(new Parameter('u', 'username', Parameter::VALUE_REQUIRED), 'The username.')
+            ->addParameter(new Parameter('H', 'host'    , '127.0.0.1')              , 'The host.')
+            ->setProgram(function($options) {
+                echo "Hello, World!\n";
+            });
+    }
+
+    public function testVersion() {
+        $result = $this->helloWorld->start(array('script.php', '-V'));
+        $this->expectOutputString(
+            "Usage: script.php -u USERNAME -p PASSWORD [OPTIONS]\n"
+            . "\n"
+            . "Say hello to the world or to a particular person\n"
+            . "\n"
+            . "  -H, --host     HOST        The host (defaults to '127.0.0.1').\n"
+            . "  -u, --username USERNAME    The username.\n"
+            . "  -p, --password PASSWORD    The password.\n"
+            . "  -h, --help                 Display this help and exit.\n"
+            . "  -V, --version              Output version information and exit.\n"
+            . "\n"
+            . "Hello v1.0\n"
+            . "Copyright (c) Copyright (c) Orange ECV 2013\n"
+        );
+    }
+
+    public function testHelp() {
+        $result = $this->helloWorld->start(array('script.php', '-h'));
+        $this->expectOutputString(
+            "Hello v1.0\n"
+            . "Copyright (c) Orange ECV 2013\n"
+        );
     }
 }
