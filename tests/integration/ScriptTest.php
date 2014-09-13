a7fad0056216684be2ca62b5b6f3c1cbde40de5f
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-09-13 19:00:36 +0200

    Added failing integration tests for issue #4

--- tests/integration/ScriptTest.php
+++ tests/integration/ScriptTest.php
@@ -15,4 +15,24 @@ class ScriptIntegrationTestCase extends AbstractCliScriptTestCase
             1
         );
     }
+
+    public function testMissingParameterValue()
+    {
+        $this->assertScriptOutput(
+            self::SCRIPT . ' -u',
+            '',
+            "Missing value for parameter -u/--username in command \"php data/test-script.php -u\"\n",
+            1
+        );
+    }
+
+    public function testConflictingParameters()
+    {
+        $this->assertScriptOutput(
+            self::SCRIPT . ' -u myname --username myname',
+            '',
+            "Conflicting parameters -u and --username in command \"php data/test-script.php -u myname --username myname\"\n",
+            1
+        );
+    }
 }
