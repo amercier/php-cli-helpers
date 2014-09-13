9f58c3a57f28ce22b0fecbe4bcf93b1001e20f82
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-09-13 18:49:44 +0200

    Replaced 'PHP Fatal error:  ' by 'Fatal error: '

--- tests/integration/ParameterTest.php
+++ tests/integration/ParameterTest.php
@@ -68,28 +68,28 @@ class ParameterIntegrationTestCase extends AbstractCliScriptTestCase
 
     public function testConflictingRequiredParameter()
     {
-        $this->assertScriptOutputStartsWith(self::SCRIPT . ' -u myname -p mypassword --username myname', '', "PHP Fatal error:  Uncaught exception 'Cli\\Helpers\\Exception\\ConflictingParameters' with message 'Conflicting parameters -u and --username in command \"php data/test-parameters.php -u myname -p mypassword --username myname\"'", 255);
+        $this->assertScriptOutputStartsWith(self::SCRIPT . ' -u myname -p mypassword --username myname', '', "Fatal error: Uncaught exception 'Cli\\Helpers\\Exception\\ConflictingParameters' with message 'Conflicting parameters -u and --username in command \"php data/test-parameters.php -u myname -p mypassword --username myname\"'", 255);
     }
 
     public function testConflictingOptionalParameter()
     {
-        $this->assertScriptOutputStartsWith(self::SCRIPT . ' -u myname -p mypassword -h myserver.example.com --host myserver.example.com', '', "PHP Fatal error:  Uncaught exception 'Cli\\Helpers\\Exception\\ConflictingParameters' with message 'Conflicting parameters -h and --host in command \"php data/test-parameters.php -u myname -p mypassword -h myserver.example.com --host myserver.example.com\"'", 255);
+        $this->assertScriptOutputStartsWith(self::SCRIPT . ' -u myname -p mypassword -h myserver.example.com --host myserver.example.com', '', "Fatal error: Uncaught exception 'Cli\\Helpers\\Exception\\ConflictingParameters' with message 'Conflicting parameters -h and --host in command \"php data/test-parameters.php -u myname -p mypassword -h myserver.example.com --host myserver.example.com\"'", 255);
     }
 
     public function testConflictingSwitchParameter()
     {
-        $this->assertScriptOutputStartsWith(self::SCRIPT . ' -u myname -p mypassword -v --verbose', '', "PHP Fatal error:  Uncaught exception 'Cli\\Helpers\\Exception\\ConflictingParameters' with message 'Conflicting parameters -v and --verbose in command \"php data/test-parameters.php -u myname -p mypassword -v --verbose\"'", 255);
+        $this->assertScriptOutputStartsWith(self::SCRIPT . ' -u myname -p mypassword -v --verbose', '', "Fatal error: Uncaught exception 'Cli\\Helpers\\Exception\\ConflictingParameters' with message 'Conflicting parameters -v and --verbose in command \"php data/test-parameters.php -u myname -p mypassword -v --verbose\"'", 255);
     }
 
     public function testMissingRequiredParameter()
     {
-        $this->assertScriptOutputStartsWith(self::SCRIPT . ''          , '', "PHP Fatal error:  Uncaught exception 'Cli\\Helpers\\Exception\\MissingRequiredParameter' with message 'Missing parameter -u/--username in command \"php data/test-parameters.php\"'", 255);
-        $this->assertScriptOutputStartsWith(self::SCRIPT . ' -u myname', '', "PHP Fatal error:  Uncaught exception 'Cli\\Helpers\\Exception\\MissingRequiredParameter' with message 'Missing parameter -p/--password in command \"php data/test-parameters.php -u myname\"'", 255);
+        $this->assertScriptOutputStartsWith(self::SCRIPT . ''          , '', "Fatal error: Uncaught exception 'Cli\\Helpers\\Exception\\MissingRequiredParameter' with message 'Missing parameter -u/--username in command \"php data/test-parameters.php\"'", 255);
+        $this->assertScriptOutputStartsWith(self::SCRIPT . ' -u myname', '', "Fatal error: Uncaught exception 'Cli\\Helpers\\Exception\\MissingRequiredParameter' with message 'Missing parameter -p/--password in command \"php data/test-parameters.php -u myname\"'", 255);
     }
 
     public function testMissingParameterValue()
     {
-        $this->assertScriptOutputStartsWith(self::SCRIPT . ' -u'        , '', "PHP Fatal error:  Uncaught exception 'Cli\\Helpers\\Exception\\MissingParameterValue' with message 'Missing value for parameter -u/--username in command \"php data/test-parameters.php -u\"'", 255);
-        $this->assertScriptOutputStartsWith(self::SCRIPT . ' --username', '', "PHP Fatal error:  Uncaught exception 'Cli\\Helpers\\Exception\\MissingParameterValue' with message 'Missing value for parameter -u/--username in command \"php data/test-parameters.php --username\"'", 255);
+        $this->assertScriptOutputStartsWith(self::SCRIPT . ' -u'        , '', "Fatal error: Uncaught exception 'Cli\\Helpers\\Exception\\MissingParameterValue' with message 'Missing value for parameter -u/--username in command \"php data/test-parameters.php -u\"'", 255);
+        $this->assertScriptOutputStartsWith(self::SCRIPT . ' --username', '', "Fatal error: Uncaught exception 'Cli\\Helpers\\Exception\\MissingParameterValue' with message 'Missing value for parameter -u/--username in command \"php data/test-parameters.php --username\"'", 255);
     }
 }
